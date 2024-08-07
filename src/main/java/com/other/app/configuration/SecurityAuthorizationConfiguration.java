6440d7457dcce8741ba3a06f5a0aca7044ac22c1
U-235-92 <a745296@yandex.ru>
2024-08-07 16:13:56 +0400

    Disable oidc

--- src/main/java/com/other/app/configuration/SecurityAuthorizationConfiguration.java
+++ src/main/java/com/other/app/configuration/SecurityAuthorizationConfiguration.java
@@ -9,18 +9,24 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -41,6 +47,11 @@ public class SecurityAuthorizationConfiguration {
 	@Bean
 	protected SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+		httpSecurity.exceptionHandling(exceptionCustomizer -> exceptionCustomizer
+				.defaultAuthenticationEntryPointFor(
+						new LoginUrlAuthenticationEntryPoint("/login"), 
+						new MediaTypeRequestMatcher(MediaType.TEXT_HTML)));
+//		httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
 		return httpSecurity.build();
 	}
 	
@@ -52,7 +63,9 @@ public class SecurityAuthorizationConfiguration {
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.redirectUri("http://localhost:8080/message_service/authorized")
+				.redirectUri("http://localhost:8080/login/oauth2/code/message_service")
+//				.scope(OidcScopes.OPENID)
+//				.scope(OidcScopes.PROFILE)
 				.scope("read_message")
 				.scope("write_message")
 				.scope("delete_message")
